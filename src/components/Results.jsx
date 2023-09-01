e83bbcaa9c7178ab83193bcef10bbc15921664f8
ivanboot <ivanma.garcia@hotmail.com>
2023-09-01 16:04:00 -0600

    Avance componente Results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,25 +1,65 @@
 import React from 'react'
 
 const Results = (props) => {
-    //Haciendo destructuring para obtener lo que viene por props
-    const {
-        nombreCliente,
-        setNombreCliente,
-        tamanioPizza,
-        setTamanioPizza,
-        precio,
-        setPrecio,
-        ingredienteAdd,
-        setIngredienteAdd,
-        costoAdicional,
-        setCostoAdicional,
-        total,
-        setTotal
-    } = props;
+  //Haciendo destructuring para obtener lo que viene por props
+  const {
+    nombreCliente,
+    setNombreCliente,
+    tamanioPizza,
+    setTamanioPizza,
+    precio,
+    setPrecio,
+    ingredienteAdd,
+    setIngredienteAdd,
+    costoAdicional,
+    setCostoAdicional,
+    total,
+    setTotal
+  } = props;
 
   return (
-    <div>Aquí se mostrarán los datos</div>
+    <>
+      <table className="table table-hover" >
+        <thead>
+          <tr>
+            <th scope="col">Concepto</th>
+            <th scope="col">Contenido</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">Nombre del cliente</th>
+            <td>{props.nombreCliente}</td>
+          </tr>
+          <tr>
+            <th scope="row">Valor de pizza {props.tamanioPizza}</th>
+            <td>${props.precio}</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Cant. ingredientes</th>
+            <td>{props.ingredienteAdd} ingredientes adicionales</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Costo extra adicional </th>
+            <td>${props.costoAdicional}</td>
+          </tr>
+
+          <tr>
+            <th scope="row">Precio total</th>
+            <td>${props.total}</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </>
   )
 }
-
+/*
+          <tr>
+            <th scope="row">Costo extra por cada ingrediente adicional</th>
+            <td>${props.costoIngreAdd}</td>
+          </tr>
+*/
 export default Results
