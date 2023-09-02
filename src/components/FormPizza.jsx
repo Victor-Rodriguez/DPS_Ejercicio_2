582555cd0902557606e8c3f32a8013b31f17f728
Victor-Rodriguez <viictor1275@gmail.com>
2023-09-01 18:28:31 -0600

    Capturando y mostrando resultados

--- src/components/FormPizza.jsx
+++ src/components/FormPizza.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Results from './Results';
 
 const FormPizza = (props) => {
     //Haciendo destructuring para obtener lo que viene por props
@@ -14,48 +15,122 @@ const FormPizza = (props) => {
         costoAdicional,
         setCostoAdicional,
         total,
-        setTotal
+        setTotal,
+        costoIngreAdd,
+        setCostoIngreAdd
     } = props;
 
-    const handleChange = e => {
+    const handleChangeNombreCliente = e => {
         e.preventDefault();
 
-        setNombreCliente({[e.target.name]: e.target.value})
-        /* setIngredienteAdd({[e.target.name]: e.target.value})
-        setTamanioPizza({[e.target.name]: e.target.value}) */
+        setNombreCliente(e.target.value)
+    }
+
+    const handleChangeTamanioPizza = e => {
+        e.preventDefault();
+
+        setTamanioPizza(e.target.value)
+    }
+    
+    const handleChangeIngredienteAdd = e => {
+        e.preventDefault();
+
+        setIngredienteAdd(e.target.value)
+    }
+
+
+    const handleClick = () => {
+        //Validaciones
+        if (tamanioPizza === 'Personal') {
+            setPrecio(7)
+            if (ingredienteAdd == 1) {
+                setCostoIngreAdd(1)
+            }else if (ingredienteAdd == 2){
+                setCostoIngreAdd(0.75)
+            }else if (ingredienteAdd == 3){
+                setCostoIngreAdd(0.50)
+            }else if (ingredienteAdd > 3) {
+                setCostoIngreAdd(0.25)
+            }
+        }else if (tamanioPizza === 'Mediana') {
+            setPrecio(10)
+            if (ingredienteAdd == 1) {
+                setCostoIngreAdd(2)
+            }else if (ingredienteAdd == 2){
+                setCostoIngreAdd(1)
+            }else if (ingredienteAdd == 3){
+                setCostoIngreAdd(0.75)
+            }else if (ingredienteAdd > 3) {
+                setCostoIngreAdd(0.50)
+            }
+        }else if (tamanioPizza === 'Grande') {
+            setPrecio(12)
+            if (ingredienteAdd == 1) {
+                setCostoIngreAdd(2.50)
+            }else if (ingredienteAdd == 2){
+                setCostoIngreAdd(2)
+            }else if (ingredienteAdd == 3){
+                setCostoIngreAdd(1)
+            }else if (ingredienteAdd > 3) {
+                setCostoIngreAdd(0.75)
+            }
+        }
+
+        //Calculando costo adicional
+        setCostoAdicional(ingredienteAdd * costoIngreAdd)
+        //Calculando total a pagar
+        setTotal(precio + costoAdicional)
 
         console.log(nombreCliente);
-        /* console.log(tamanioPizza);
-        console.log(ingredienteAdd); */
+        console.log(ingredienteAdd);
+        console.log(tamanioPizza);
+
     }
 
 
     return (
         <>
-            <div class='container'>
+            <div className='container'>
                 <h1>Ordene su Pizza</h1>
-                <div class="row justify-content-center">
+                <div className="row justify-content-center">
                     <form onSubmit={e => e.preventDefault()}>
-                        <div class="mb-3">
-                            <label for="nombreCliente" class="form-label">Nombre del Cliente:</label>
-                            <input type="text" class="form-control" name='nombreCliente'  onChange={handleChange} />
+                        <div className="mb-3">
+                            <label htmlFor="nombreCliente" className="form-label">Nombre del Cliente:</label>
+                            <input type="text" className="form-control" name='nombreCliente'  onChange={handleChangeNombreCliente} />
                         </div>
-                        <div class="mb-3">
-                            <select class="form-select" name='tamanioPizza'  >
-                                <option selected>Seleccione el tamaño</option>
+                        <div className="mb-3">
+                            <select className="form-select" name='tamanioPizza'  onChange={handleChangeTamanioPizza}>
+                                <option >Seleccione el tamaño</option>
                                 <option value="Personal">Personal</option>
                                 <option value="Mediana">Mediana</option>
                                 <option value="Grande">Grande</option>
                             </select>
                         </div>
-                        <div class="mb-3">
-                            <label for="ingredienteAdd" class="form-label">Ingredientes adicionales</label>
-                            <input type="text" class="form-control" name='ingredienteAdd'  />
+                        <div className="mb-3">
+                            <label htmlFor="ingredienteAdd" className="form-label">Ingredientes adicionales</label>
+                            <input type="text" className="form-control" name='ingredienteAdd' onChange={handleChangeIngredienteAdd}/>
                         </div>
-                        <button type="submit" class="btn btn-primary">Enviar</button>
+                        <button type="submit" className="btn btn-primary" onClick={handleClick} >Enviar</button>
                     </form>
 
                 </div>
+                <Results
+                    nombreCliente={nombreCliente}
+                    setNombreCliente={setNombreCliente}
+                    tamanioPizza={tamanioPizza}
+                    setTamanioPizza={setTamanioPizza}
+                    precio={precio}
+                    setPrecio={setPrecio}
+                    ingredienteAdd={ingredienteAdd}
+                    setIngredienteAdd={setIngredienteAdd}
+                    costoAdicional={costoAdicional}
+                    setCostoAdicional={setCostoAdicional}
+                    total={total}
+                    setTotal={setTotal}
+                    costoIngreAdd={costoIngreAdd}
+                    setCostoIngreAdd={setCostoIngreAdd}
+                  />
+
             </div>
         </>
     )
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,60 +1,67 @@
 import React from 'react'
 
 const Results = (props) => {
-  //Haciendo destructuring para obtener lo que viene por props
-  const {
-    nombreCliente,
-    setNombreCliente,
-    tamanioPizza,
-    setTamanioPizza,
-    precio,
-    setPrecio,
-    ingredienteAdd,
-    setIngredienteAdd,
-    costoAdicional,
-    setCostoAdicional,
-    total,
-    setTotal
-  } = props;
+    //Haciendo destructuring para obtener lo que viene por props
+    const {
+        nombreCliente,
+        setNombreCliente,
+        tamanioPizza,
+        setTamanioPizza,
+        precio,
+        setPrecio,
+        ingredienteAdd,
+        setIngredienteAdd,
+        costoAdicional,
+        setCostoAdicional,
+        total,
+        setTotal,
+        costoIngreAdd,
+        setCostoIngreAdd
+    } = props;
 
-  return (
-    <>
-      <table className="table table-hover" >
-        <thead>
-          <tr>
-            <th scope="col">Concepto</th>
-            <th scope="col">Contenido</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">Nombre del cliente</th>
-            <td>{props.nombreCliente}</td>
-          </tr>
-          <tr>
-            <th scope="row">Valor de pizza {props.tamanioPizza}</th>
-            <td>${props.precio}</td>
-          </tr>
+    return (
+        <>
+            <table className="table table-hover" >
+                <thead>
+                    <tr>
+                        <th scope="col">Concepto</th>
+                        <th scope="col">Contenido</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Nombre del cliente</th>
+                        <td>{nombreCliente}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Valor de pizza {props.tamanioPizza}</th>
+                        <td>${precio}</td>
+                    </tr>
 
-          <tr>
-            <th scope="row">Cant. ingredientes</th>
-            <td>{props.ingredienteAdd} ingredientes adicionales</td>
-          </tr>
+                    <tr>
+                        <th scope="row">Cant. ingredientes</th>
+                        <td>{ingredienteAdd} ingredientes adicionales</td>
+                    </tr>
 
-          <tr>
-            <th scope="row">Costo extra adicional </th>
-            <td>${props.costoAdicional}</td>
-          </tr>
+                    <tr>
+                        <th scope="row">Costo extra por cada ingrediente adicional</th>
+                        <td>${costoIngreAdd}</td>
+                    </tr>
 
-          <tr>
-            <th scope="row">Precio total</th>
-            <td>${props.total}</td>
-          </tr>
+                    <tr>
+                        <th scope="row">Costo extra adicional </th>
+                        <td>${costoAdicional}</td>
+                    </tr>
 
-        </tbody>
-      </table>
-    </>
-  )
+                    <tr>
+                        <th scope="row">Precio total</th>
+                        <td>${total}</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </>
+    )
 }
 /*
           <tr>
